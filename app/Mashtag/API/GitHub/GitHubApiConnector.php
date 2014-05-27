5b27942fa45b652fb228e1cc36057718faf3e3bd
Ryan O'Hara <ryan@ryanohara.co>
2014-05-27 14:28:05 -0400

    GitHub support

--- app/Mashtag/API/GitHub/GitHubApiConnector.php
+++ app/Mashtag/API/GitHub/GitHubApiConnector.php
@@ -2,23 +2,13 @@
 
 use \Guzzle\Http\Client as Client;
 
-abstract class GooglePlusApiConnector {
+abstract class GitHubApiConnector {
 
 	protected static $baseUrl = "https://api.github.com";
-	protected static $apiVersion = "v3";
-	protected $key;
 
 	public function __construct() {
 
 	}
-
-	protected function getKey() {
-		if (!$this->key) {
-			$this->key = getenv('GOOGLE_PLUS_KEY');
-		}
-		return $this->key;
-	}
-
 
 	/**
 	 * Perform an API request
@@ -28,46 +18,13 @@ abstract class GooglePlusApiConnector {
 	 */
 
 	public function doRequest($method, $queryString) {
-		$url = self::$baseUrl . "/" . self::$apiVersion . "/" . $queryString;
+		$url = self::$baseUrl . "/" . $queryString;
 		$client = new Client($url);
 
 		$response = $client->$method()->send();
 
-		# Decompress gzip response
+		# Return response
 		return $response->getBody(true);
-	}
-
-}
-
-<?php namespace Mashtag\API\StackExchange;
-
-use \Guzzle\Http\Client as Client;
-
-abstract class StackExchangeApiConnector {
-
-	protected static $baseUrl = "https://api.stackexchange.com";
-	protected static $apiVersion = "2.2";
-
-	public function __construct() {
-
-	}
-
-
-	/**
-	 * Perform an API request
-	 * @param string $method 
-	 * @param string $queryString 
-	 * @return json
-	 */
-
-	public function doRequest($method, $queryString) {
-		$url = self::$baseUrl . "/" . self::$apiVersion . "/" . $queryString;
-		$client = new Client($url);
-
-		$response = $client->$method()->send();
-
-		# Decompress gzip response
-		return gzinflate(substr($response->getBody(true), 10, -8));
 	}
 
 }
